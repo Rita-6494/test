2b6551bf8eb0cd7f7d2a504afd09bbe2e3cf3f78
Rita <margaeryolhovik@yandex.ru>
2024-01-17 23:01:03 +0400

    First commit

---
+++ src/Svetofor.java
@@ -0,0 +1,12 @@
+public class Svetofor {
+    public static void main(String[] arg) {
+        String signal = "green";
+        if (signal.equals("red")) {
+            System.out.println("Остановиться!");
+        } else if (signal.equals("yellow")) {
+            System.out.println("Подождать");
+        } else {
+            System.out.println("Ехать");
+        }
+    }
+}
